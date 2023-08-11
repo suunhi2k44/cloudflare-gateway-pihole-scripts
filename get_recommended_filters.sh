c2ab073e2011e16b1db2bd6f1b8e87a76ca54621
suunhi2k44 <141858611+suunhi2k44@users.noreply.github.com>
2023-08-11 20:39:41 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -5,7 +5,10 @@ touch input.csv
 
 # declare an array of urls
 urls=(
+  https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
+  https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/multi.txt
+  
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
