bcd53b7f82691e571cb3f9a353057aea054dd3c4
suunhi2k44 <141858611+suunhi2k44@users.noreply.github.com>
2023-08-16 09:55:07 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -5,10 +5,6 @@ touch input.csv
 
 # declare an array of urls
 urls=(
-  
-  https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
-  
-  
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
